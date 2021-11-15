0f8ad09c244c87ec24300a3597d5727a1ca846b9
Little-T-Mouse <92545115+Little-T-Mouse@users.noreply.github.com>
2021-11-15 17:57:56 -0500

    Create Mathematical Algorithm - isprime.java

---
+++ 02 Mathematical Algorithm/Mathematical Algorithm - isprime.java
@@ -0,0 +1,9 @@
+import static java.lang.Math.*;
+public boolean isprime(int a) {
+		if(a < 2) return false;
+		if(a == 2) return true;
+		for(int i=0; i<Math.sqrt(a); i++) {
+			if(a%i==0) return false;
+		}
+		return true;
+}
