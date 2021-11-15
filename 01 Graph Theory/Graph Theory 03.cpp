9cf28b17854e1526b55ad19044c9ff0972d37261
Little-T-Mouse <92545115+Little-T-Mouse@users.noreply.github.com>
2021-11-15 16:16:45 -0500

    Create Graph Theory 03.cpp

---
+++ 01 Graph Theory/Graph Theory 03.cpp
@@ -0,0 +1,16 @@
+int bfs_farest_node(int from){ // Find the farest node from a node
+    int level[N]; queue<int> que; bool vis[N];
+    memset(vis, false, sizeof(vis)); que.push(start); memset(level, 0, sizeof(level)); int farest=-1;
+    while(!que.empty()){
+        int node = que.front(); que.pop();
+        for(int next: graph[node]){
+            if(!vis[next]){
+                vis[next] = true;
+                que.push(next);
+                level[next] = level[node]+1;
+                if(level[next]>farest) farest = level[next];
+            }
+        }
+    }
+    return farest;
+}
