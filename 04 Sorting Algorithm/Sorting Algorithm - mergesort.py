b6ed90942d349bc2be6b167aa932ea132d19a1d5
Little-T-Mouse <92545115+Little-T-Mouse@users.noreply.github.com>
2021-11-15 22:15:46 -0500

    Create Sorting Algorithm - mergesort.py

---
+++ 04 Sorting Algorithm/Sorting Algorithm - mergesort.py
@@ -0,0 +1,23 @@
+def mergesort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left, right = arr[:mid], arr[mid:]
+        mergesort(left)
+        mergesort(right)
+        i = j = k = 0
+        while i < len(left) and j < len(right):
+            if left[i] > right[j]:
+                arr[k] = right[j]
+                j += 1
+            else:
+                arr[k] = left[i]
+                i += 1
+            k += 1
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1; k += 1
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1; k += 1
+    return arr
+  # Note: This is an Antt(Anthony-Sheu) Style merge-sort.
