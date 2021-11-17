21ed5a3f8e38894d9df2b733a5b712edec4729fe
Little-T-Mouse <92545115+Little-T-Mouse@users.noreply.github.com>
2021-11-16 21:11:22 -0500

    Create Mathematical Algorithm - fib(dynamic).cpp

---
+++ 02 Mathematical Algorithm/Mathematical Algorithm - fib(dynamic).cpp
@@ -0,0 +1,12 @@
+// Dynamic Programming Version
+int fib(int n){
+    int f[n + 2];
+    int i;
+    f[0] = 0;
+    f[1] = 1;
+ 
+    for(i = 2; i <= n; i++){
+       f[i] = f[i - 1] + f[i - 2];
+    }
+    return f[n];
+ }
