d0ec6186ba766dbfd51234a92d292d5eb0450389
Little-T-Mouse <92545115+Little-T-Mouse@users.noreply.github.com>
2021-11-20 14:43:19 -0500

    Update Mathematical Algorithm - fib(recursion_2).py

--- 02 Mathematical Algorithm/Mathematical Algorithm - fib(recursion_2).py
+++ 02 Mathematical Algorithm/Mathematical Algorithm - fib(recursion_2).py
@@ -1,13 +1,11 @@
 import sys
 import math
 def mp(n):
-        if n==1:
-            return 1,1,0
+        if n==1: return 1,1,0
         else:
             a,b,c = mp(n//2)
             a,b,c = ((a**2+b**2)%MOD,(a*b+b*c)%MOD,(b**2+c**2)%MOD)
-        if n%2==1:
-            a,b,c = ((a+b)%MOD,a,b)
+        if n%2==1: a,b,c = ((a+b)%MOD,a,b)
         return a,b,c
 
 MOD = int(1e9)+7
