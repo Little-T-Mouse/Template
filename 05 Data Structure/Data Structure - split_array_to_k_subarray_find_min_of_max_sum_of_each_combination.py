84fada34bc46826f689a9872d6ea8d1bd4b0ecdf
Little-T-Mouse <92545115+Little-T-Mouse@users.noreply.github.com>
2021-11-21 20:47:54 -0500

    Update Data Structure - split_array_to_k_subarray_find_min_of_max_sum_of_each_combination.py

--- 05 Data Structure/Data Structure - split_array_to_k_subarray_find_min_of_max_sum_of_each_combination.py
+++ 05 Data Structure/Data Structure - split_array_to_k_subarray_find_min_of_max_sum_of_each_combination.py
@@ -2,6 +2,7 @@
 '''
 Split the given array into K sub-arrays such that maximum sum of all sub arrays is minimum.
 '''
+# Version 1:
 def check(mid, array, n, K):
     count = 0
     sum = 0
@@ -34,5 +35,29 @@ N, K = list(map(int, input().split()))
 l = list(map(int, input().split()))
 
 print(solve(l, N, K))
+#######################################
+# Version 2: Antt (Anthoy Sheu)
+def solve():
+    s, e = max(f), sum(f)
+    ans = 0
+    while s <= e:
+        mid = (s+e)//2
+        if check(mid): ans = mid; e = mid-1
+        else: s = mid+1
+    return ans
+
+def check(mid):
+    c, s = 0, 0
+    for i in range(n):
+        if f[i] > mid: return 0
+        s += f[i]
+        if s > mid: c += 1; s = f[i]
+    c += 1
+    return c <= m
+
+n, m = list(map(int, input().split()))
+f = list(map(int, input().split()))
+
+print(solve())
 
 # Note: From https://www.geeksforgeeks.org/split-the-given-array-into-k-sub-arrays-such-that-maximum-sum-of-all-sub-arrays-is-minimum/
