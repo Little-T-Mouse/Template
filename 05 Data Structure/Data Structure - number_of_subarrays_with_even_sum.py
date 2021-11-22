154f2a59b3343e0f99d01863cd3d9c1f857bbf69
Little-T-Mouse <92545115+Little-T-Mouse@users.noreply.github.com>
2021-11-21 21:16:34 -0500

    Create Data Structure - number_of_subarrays_with_even_sum.py

---
+++ 05 Data Structure/Data Structure - number_of_subarrays_with_even_sum.py
@@ -0,0 +1,15 @@
+# The Number of Subarrays with even sum.
+ef cntEvenSum(arr, n):
+    res = 0
+    s = 0 
+    for i in reversed(range(n)):
+        if arr[i] % 2 == 1:
+            s = n-i-1-s
+        else:
+            s = s+1
+        res = res + s
+    return res
+
+arr = [1, 2, 2, 3, 4, 1]
+n = len(arr)
+print("The Number of Subarrays with even sum is", countEvenSum(arr, n))
