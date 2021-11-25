410efb01f9e25acd2839ef35d4990d4f2223ba46
Little-T-Mouse <92545115+Little-T-Mouse@users.noreply.github.com>
2021-11-25 09:29:44 -0500

    Update and rename Mathematical Algorithm - quickpow2.cpp to Mathematical Algorithm - quickpow & quickmul.cpp

--- 02 Mathematical Algorithm/Mathematical Algorithm - quickpow & quickmul.cpp
+++ 02 Mathematical Algorithm/Mathematical Algorithm - quickpow & quickmul.cpp
@@ -1,3 +1,7 @@
+ll quickMul(ll x, ll y, ll mod) {
+    if (y==0) return 0;
+    return (x*(y&1)+quickMul((x+x)%mod,y>>1,mod))%mod;
+}
 ll quickPow(ll x, ll y, ll mod) {
     if (y==0) return 1;
     return quickMul((y&1?x:1),quickPow(quickMul(x,x,mod),y>>1,mod),mod);
