42d83d41d3b52ed259d326d44ce8ab0b24c193b9
Little-T-Mouse <92545115+Little-T-Mouse@users.noreply.github.com>
2021-12-16 13:59:42 -0500

    Update Searching Algorithm - binary_search.cpp

--- 07 Searching Algorithm/Searching Algorithm - binary_search.cpp
+++ 07 Searching Algorithm/Searching Algorithm - binary_search.cpp
@@ -1,9 +1,25 @@
+// 1
 int binarySearch(int arr[], int l, int r, int x){
     while (l <= r) {
         int m = l + (r - l) / 2;
-        if (arr[m] == x) return m;
-        if (arr[m] < x) l = m + 1;
+        if (condition) return m;
+        if (condition_2) l = m + 1;
         else r = m - 1;
     }
     return -1;
+}
+// 2
+while(l<=r){
+     mid = (l+r)/2;
+     if(condition){
+       r = mid-1;
+     }else{
+       l = mid+1;
+     }
+}
+// 3
+while(l<r){
+      mid = (l+r)/2;
+      if(condition)r=mid;
+      else l = mid+1;
 }
