61e4fe38952fd0b51e8e882e09f2dadb0eb1183e
Little-T-Mouse <92545115+Little-T-Mouse@users.noreply.github.com>
2021-12-24 08:50:57 -0500

    Update Mathematical Algorithm - isprime.py

--- 02 Mathematical Algorithm/Mathematical Algorithm - isprime.py
+++ 02 Mathematical Algorithm/Mathematical Algorithm - isprime.py
@@ -3,5 +3,5 @@ def isprime(a):
     if a < 2: return False
     elif a == 2: return True
     for i in range(2, math.sqrt(a)):
-        if a%i!=0: return False
+        if a%i==0: return False
     return True
