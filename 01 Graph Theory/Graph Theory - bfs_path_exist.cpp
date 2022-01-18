2a2042b9f5d0e0b112854f2368be978d83136140
Little-T-Mouse <92545115+Little-T-Mouse@users.noreply.github.com>
2022-01-18 09:19:45 -0500

    Update Graph Theory - bfs_path_exist.cpp

--- 01 Graph Theory/Graph Theory - bfs_path_exist.cpp
+++ 01 Graph Theory/Graph Theory - bfs_path_exist.cpp
@@ -1,6 +1,6 @@
 bool bfs_path_exist(int from, int to){ // Find if there are a path between two point
     queue<int> que; bool vis[N];
-    memset(vis, false, sizeof(vis)); que.push(start);
+    memset(vis, false, sizeof(vis)); que.push(from);
     while(!que.empty()){
         int node = que.front(); que.pop();
         if(node == to) return true;
